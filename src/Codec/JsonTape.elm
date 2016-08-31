07240c470a019197a01979ac0e103c2bb8a18432
Jan Tojnar <jtojnar@gmail.com>
2016-08-31 16:28:17 +0200

    add functions for getting file content

--- src/Codec/JsonTape.elm
+++ src/Codec/JsonTape.elm
@@ -1,16 +1,26 @@
-module Codec.JsonTape exposing (DirTree(..), getName)
+module Codec.JsonTape exposing (DirTree(..), getName, getObject, readFileRaw, readFile)
 
-{-| This module describes data structures representing files and directories.
+{-| This module describes data structures representing files and directories and functions for manipulating them.
 
 # Data structure
 @docs DirTree
+
+# File manipulation
+@docs getObject, readFileRaw, readFile
 
 # Helpers
 @docs getName
 -}
 
+import Base64
+import String
+
 
 type alias FileName =
+    String
+
+
+type alias Path =
     String
 
 
@@ -27,6 +37,75 @@ type DirTree
         }
 
 
+
+-- FILE MANIPULATION
+
+
+{-| Traverse the directory tree along path and return the seeked object.
+-}
+getObject : Path -> DirTree -> Result String DirTree
+getObject path tree =
+    get (String.split "/" path) [ tree ]
+
+
+{-| Traverse the directory tree along path and return the content of the found file.
+-}
+readFileRaw : Path -> DirTree -> Result String String
+readFileRaw path tree =
+    getObject path tree
+        `Result.andThen`
+            (\obj ->
+                case obj of
+                    Dir _ ->
+                        Err "Provided path is a directory."
+
+                    File { file } ->
+                        Ok file
+            )
+
+
+{-| Traverse the directory tree along path and return the content of the found file after base64 decoding it.
+-}
+readFile : Path -> DirTree -> Result String String
+readFile path tree =
+    readFileRaw path tree `Result.andThen` Base64.decode
+
+
+
+-- HELPERS
+
+
+{-| Auxilliary function that traverses the directory tree using path segments.
+-}
+get : List FileName -> List DirTree -> Result String DirTree
+get nav level =
+    case nav of
+        cur :: rest ->
+            case find cur level of
+                Just node ->
+                    if List.isEmpty rest then
+                        Ok node
+                    else
+                        get rest (getChildren node)
+
+                Nothing ->
+                    Err "There is no such path in the tree."
+
+        [] ->
+            List.head level |> Result.fromMaybe "This should not happen"
+
+
+{-| Auxilliary functions that searches for a file inside a list of files.
+-}
+find : FileName -> List DirTree -> Maybe DirTree
+find searchedName contents =
+    List.head
+        (List.filter
+            (\f -> getName f == searchedName)
+            contents
+        )
+
+
 {-| Auxilliary function for circumventing the type system.
 -}
 getName : DirTree -> FileName
@@ -37,3 +116,15 @@ getName node =
 
         File { name } ->
             name
+
+
+{-| Auxilliary function for getting children of the node; files have no children.
+-}
+getChildren : DirTree -> List DirTree
+getChildren node =
+    case node of
+        Dir { contents } ->
+            contents
+
+        File _ ->
+            []
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,7 +2,6 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
 import Codec.JsonTape exposing (..)
 import Codec.JsonTape.Decode as DecodeTape
 import Codec.JsonTape.Encode as EncodeTape
@@ -23,6 +22,39 @@ all =
             , test "decoding of invalid json-tape" <|
                 \() ->
                     (Json.decodeString DecodeTape.dirTree sampleJsonBadKind) |> Expect.equal (Err "I ran into a `fail` decoder: Unknown kind: badkind")
+            ]
+        , describe "Directory tree manipulation"
+            [ describe "Getting a tree object"
+                [ test "basic file object" <|
+                    \() ->
+                        (getObject "antigravity-0.1/PKG-INFO" sampleTree) |> Expect.equal (Ok sampleFileObject)
+                , test "basic directory object" <|
+                    \() ->
+                        (getObject "antigravity-0.1/antigravity" sampleTree) |> Expect.equal (Ok sampleDirectoryObject)
+                , test "reading non-existing path" <|
+                    \() ->
+                        (getObject "antigravity-0.1/foobar" sampleTree) |> Expect.equal (Err "There is no such path in the tree.")
+                , test "attempting to descend through file" <|
+                    \() ->
+                        (getObject "antigravity-0.1/PKG-INFO/baz" sampleTree) |> Expect.equal (Err "There is no such path in the tree.")
+                , test "traversing a deep tree" <|
+                    \() ->
+                        (getObject "a/b/c/d/e/f" deepTree) |> Expect.equal (Ok deepObject)
+                ]
+            , describe "Reading a file"
+                [ test "basic reading" <|
+                    \() ->
+                        (readFileRaw "antigravity-0.1/PKG-INFO" sampleTree) |> Expect.equal (Ok sampleFileContent)
+                , test "reading a directory" <|
+                    \() ->
+                        (readFileRaw "antigravity-0.1/antigravity" sampleTree) |> Expect.equal (Err "Provided path is a directory.")
+                , test "reading non-existing path" <|
+                    \() ->
+                        (readFileRaw "antigravity-0.1/foobar" sampleTree) |> Expect.equal (Err "There is no such path in the tree.")
+                , test "attempting to descend through file" <|
+                    \() ->
+                        (readFileRaw "antigravity-0.1/PKG-INFO/baz" sampleTree) |> Expect.equal (Err "There is no such path in the tree.")
+                ]
             ]
         ]
 
@@ -52,27 +84,80 @@ sampleTree =
     Dir
         { name = "antigravity-0.1"
         , contents =
-            [ File
-                { name = "PKG-INFO"
-                , file =
-                    "TWV0YWRhdGEtVmVyc2lvbjogMS4wDQpOYW1lOiBhbnRpZ3Jhdml0eQ0KVmVyc2lvbjogMC4xDQpTdW1tYXJ5OiBBIHJlYWxseSBzaW1wbGUgbW9kdWxlIHRoYXQgYWxsb3cgZXZlcnlvbmUgdG8gZG8gImltcG9ydCBhbnRpZ3Jhdml0eSINCkhvbWUtcGFnZTogaHR0cDovL2ZhYmllbi5zY2h3b2Iub3JnL2FudGlncmF2aXR5Lw0KQXV0aG9yOiBGYWJpZW4gU2Nod29iDQpBdXRob3ItZW1haWw6IGFudGlncmF2aXR5QHgtcGh1dHVyZS5jb20NCkxpY2Vuc2U6IFVOS05PV04NCkRlc2NyaXB0aW9uOiBVTktOT1dODQpQbGF0Zm9ybTogVU5LTk9XTg0K"
-                }
-            , Dir
-                { name = "antigravity"
-                , contents =
-                    [ File
-                        { name = "__init__.py"
-                        , file =
-                            "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uDQoNClNUUklQX1VSTCA9ICJodHRwOi8veGtjZC5jb20vMzUzLyINCg0KZGVmIHN0YXJ0KCk6DQogICAgcmV0dXJuIFNUUklQX1VSTA=="
-                        }
-                    ]
-                }
+            [ sampleFileObject
+            , sampleDirectoryObject
             , File
                 { name = "setup.py"
                 , file =
                     "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uDQoNCmZyb20gZGlzdHV0aWxzLmNvcmUgaW1wb3J0IHNldHVwDQoNCnNldHVwKA0KICAgIG5hbWU9J2FudGlncmF2aXR5JywNCiAgICB2ZXJzaW9uPScwLjEnLA0KICAgIGRlc2NyaXB0aW9uPSdBIHJlYWxseSBzaW1wbGUgbW9kdWxlIHRoYXQgYWxsb3cgZXZlcnlvbmUgdG8gZG8gImltcG9ydCBhbnRpZ3Jhdml0eSInLA0KICAgIGF1dGhvcj0nRmFiaWVuIFNjaHdvYicsDQogICAgYXV0aG9yX2VtYWlsPSdhbnRpZ3Jhdml0eUB4LXBodXR1cmUuY29tJywNCiAgICB1cmw9J2h0dHA6Ly9mYWJpZW4uc2Nod29iLm9yZy9hbnRpZ3Jhdml0eS8nLA0KICAgIHBhY2thZ2VzPVsnYW50aWdyYXZpdHknXSwNCik="
                 }
             ]
+        }
+
+
+sampleFileContent : String
+sampleFileContent =
+    "TWV0YWRhdGEtVmVyc2lvbjogMS4wDQpOYW1lOiBhbnRpZ3Jhdml0eQ0KVmVyc2lvbjogMC4xDQpTdW1tYXJ5OiBBIHJlYWxseSBzaW1wbGUgbW9kdWxlIHRoYXQgYWxsb3cgZXZlcnlvbmUgdG8gZG8gImltcG9ydCBhbnRpZ3Jhdml0eSINCkhvbWUtcGFnZTogaHR0cDovL2ZhYmllbi5zY2h3b2Iub3JnL2FudGlncmF2aXR5Lw0KQXV0aG9yOiBGYWJpZW4gU2Nod29iDQpBdXRob3ItZW1haWw6IGFudGlncmF2aXR5QHgtcGh1dHVyZS5jb20NCkxpY2Vuc2U6IFVOS05PV04NCkRlc2NyaXB0aW9uOiBVTktOT1dODQpQbGF0Zm9ybTogVU5LTk9XTg0K"
+
+
+sampleFileObject : DirTree
+sampleFileObject =
+    File
+        { name = "PKG-INFO"
+        , file = sampleFileContent
+        }
+
+
+sampleDirectoryObject : DirTree
+sampleDirectoryObject =
+    Dir
+        { name = "antigravity"
+        , contents =
+            [ File
+                { name = "__init__.py"
+                , file =
+                    "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uDQoNClNUUklQX1VSTCA9ICJodHRwOi8veGtjZC5jb20vMzUzLyINCg0KZGVmIHN0YXJ0KCk6DQogICAgcmV0dXJuIFNUUklQX1VSTA=="
+                }
+            ]
+        }
+
+
+deepTree : DirTree
+deepTree =
+    Dir
+        { name = "a"
+        , contents =
+            [ Dir
+                { name = "b"
+                , contents =
+                    [ Dir
+                        { name = "c"
+                        , contents =
+                            [ Dir
+                                { name = "d"
+                                , contents =
+                                    [ Dir
+                                        { name = "e"
+                                        , contents =
+                                            [ deepObject
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+
+deepObject : DirTree
+deepObject =
+    File
+        { name = "f"
+        , file =
+            "baz"
         }
 
 
